63ffa32a41f6ebf5ffb470725d79f0ba459dfd36
Sanket Belkhede <sanketbelkhede01.com>
2025-06-25 16:06:50 +0530

    @Component directive explained

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-add-employee',
-  standalone: true,
+  selector: 'app-add-employee', // unique identifier
+  standalone: true, // from angular 17, independent components, no need to register in app.module.ts
   imports: [],
-  templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.css'
+  templateUrl: './add-employee.component.html', // we can use template also
+  styleUrl: './add-employee.component.css' // we can use styles also
 })
 export class AddEmployeeComponent {
 
